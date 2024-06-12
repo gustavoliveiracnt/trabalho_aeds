33d7adaaf7685caee4809e8dc2d78500ff20b227
Isadora Xavier <122477054+isadoraxaavier@users.noreply.github.com>
2024-06-12 18:41:38 -0300

    Add files via upload

---
+++ Entities/Curso.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trabalho_Pratico.Entities
+{
+    internal class Curso
+    {
+        private int id;
+        private string nome;
+        private int numVagas;
+        private double notaCorte;
+        private List<Candidato> candidatosSelecionados = new List<Candidato>();
+        private FilaFlexivel filaEspera = new FilaFlexivel();
+
+        public Curso() { }
+
+        public double NotaCorte
+        {
+            get { return notaCorte; }
+            set { notaCorte = value; }
+        }
+
+        public List<Candidato> CandidatosSelecionados
+        {
+            get { return candidatosSelecionados; }
+            set { candidatosSelecionados = value; }
+        }
+
+        public FilaFlexivel FilaEspera
+        {
+            get { return filaEspera; }
+            set { filaEspera = value; }
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public int NumVagas
+        {
+            get { return numVagas; }
+            set { numVagas = value; }
+        }
+
+
+        public Curso(int id, string nome, int quantidadeAlunos)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.numVagas = quantidadeAlunos;
+        }
+
+        public void Merge(List<Candidato> candidatos, int esq, int meio, int dir)
+        {
+            int nEsq = meio - esq + 1;
+            int nDir = dir - meio;
+
+            List<Candidato> listEsq = new List<Candidato>(nEsq + 1);
+            List<Candidato> listDir = new List<Candidato>(nDir + 1);
+
+
+            for (int j = 0; j <= nEsq; j++) listEsq.Add(new Candidato());
+    for (int j = 0; j <= nDir; j++) listDir.Add(new Candidato());
+
+
+            listEsq[nEsq].NotaMedia = int.MinValue;
+            listDir[nDir].NotaMedia = int.MinValue;
+
+            int iEsq, iDir, i;
+
+            for (iEsq = 0; iEsq < nEsq; iEsq++)
+            {
+                listEsq[iEsq] = candidatos[esq + iEsq];
+            }
+
+            for (iDir = 0; iDir < nDir; iDir++)
+            {
+                listDir[iDir] = candidatos[meio + 1 + iDir];
+            }
+
+            for (i = esq, iEsq = iDir = 0; i <= dir; i++)
+            {
+                if (listEsq[iEsq].NotaMedia > listDir[iDir].NotaMedia || (listEsq[iEsq].NotaMedia == listDir[iDir].NotaMedia && listEsq[iEsq].NotaRed > listDir[iDir].NotaRed))
+                {
+                    candidatos[i] = listEsq[iEsq];
+                    iEsq++;
+                }
+                else
+                {
+                    candidatos[i] = listDir[iDir];
+                    iDir++;
+                }
+
+            }
+        }
+
+        public void MergeSort(List<Candidato> candidatos, int esq, int dir)
+        {
+            if (esq < dir)
+            {
+                int meio = (esq + dir) / 2;
+                MergeSort(candidatos, esq, meio);
+                MergeSort(candidatos, meio + 1, dir);
+                Merge(candidatos, esq, meio, dir);
+            }
+        }
+
+        
+
+
+        public string Imprimir()
+        {
+            string arquivo = "";
+            int position = CandidatosSelecionados.Count - 1;
+            this.notaCorte = candidatosSelecionados[position].NotaMedia;
+
+            Console.WriteLine($"{this.nome} {this.notaCorte.ToString("N2")}");
+            Console.WriteLine("Selecionados");
+
+            arquivo += $"{this.nome} {this.notaCorte.ToString("N2")}" + "\n" + "Selecionados" + "\n";
+
+            for (int i = 0; i < candidatosSelecionados.Count; i++)
+            {
+                Console.WriteLine($"{candidatosSelecionados[i].NomeCandidato} {candidatosSelecionados[i].NotaMedia.ToString("N2")}");
+
+                arquivo += $"{candidatosSelecionados[i].NomeCandidato} {candidatosSelecionados[i].NotaMedia.ToString("N2")}" + "\n";
+            }
+
+            Console.WriteLine("Fila de Espera");
+
+            arquivo += "Fila de Espera" + "\n";
+
+            filaEspera.Mostrar(ref arquivo);
+            Console.WriteLine();
+
+            return arquivo;
+        }
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Trabalho_Pratico.Entities;
+
+
+namespace Trabalho_Pratico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string listaDeImpressao = "";
+            Console.WriteLine("Informe caminho do arquivo: ");
+            string path = Console.ReadLine();
+            StreamReader arquivo = new StreamReader($@"{path}", Encoding.UTF8);
+            string[] dados;
+            int qtdCursos, qtdCandidatos;
+            Dictionary<int, Curso> curso = new Dictionary<int, Curso>();
+            List<Candidato> candidatos = new List<Candidato>();
+            try
+            {
+                string leitura = arquivo.ReadLine();
+                dados = leitura.Split(';');
+                qtdCursos = int.Parse(dados[0]);
+                qtdCandidatos = int.Parse(dados[1]);
+                int contador = 0;
+                leitura = arquivo.ReadLine();
+                do
+                {
+                    dados = leitura.Split(';');
+                    if (contador < qtdCursos)
+                    {
+                        int id = int.Parse(dados[0]);
+                        string nome = dados[1];
+                        int qtdAunos = int.Parse(dados[2]);
+                        curso.Add(id, new Curso(id, nome, qtdAunos));
+                    }
+                    else
+                    {
+                        string nome = dados[0];
+                        int notaRed = int.Parse(dados[1]);
+                        int notaMat = int.Parse(dados[2]);
+                        int notaLing = int.Parse(dados[3]);
+                        int cursoUm = int.Parse(dados[4]);
+                        int cursoDois = int.Parse(dados[5]);
+                        candidatos.Add(new Candidato(nome, notaRed, notaMat, notaLing, cursoUm, cursoDois));
+                    }
+                    contador++;
+                    leitura = arquivo.ReadLine();
+                } while (leitura != null);
+
+                Curso cursotemp = new Curso();
+                cursotemp.MergeSort(candidatos, 0, candidatos.Count - 1);
+
+                for (int i = 0; i < candidatos.Count; i++)
+                {
+                    SelecaoDeAlunos(candidatos[i], curso);
+                }
+
+                for (int i = 1; i <= qtdCursos; i++)
+                {
+                    listaDeImpressao += curso[i].Imprimir();
+                }
+
+                GerarArquivo(listaDeImpressao);
+
+
+
+
+            }
+            catch (IOException exception)
+            {
+                Console.WriteLine("Erro inesperado: " + exception.Message);
+            }
+
+
+            Console.ReadLine();
+        }
+        public static void SelecaoDeAlunos(Candidato candidato, Dictionary<int, Curso> cursos)
+
+        {
+            double notacandidato = candidato.NotaMedia;
+            int id1 = candidato.CodCursoUm;
+            int id2 = candidato.CodCursoDois;
+
+            if (cursos[id1].CandidatosSelecionados.Count < cursos[id1].NumVagas)
+            {
+
+                cursos[id1].CandidatosSelecionados.Add(candidato);
+            }
+
+            else
+            {
+                cursos[id1].FilaEspera.Inserir(candidato);
+
+                if (cursos[id2].CandidatosSelecionados.Count < cursos[id2].NumVagas)
+                {
+                    cursos[id2].CandidatosSelecionados.Add(candidato);
+                }
+                else
+                {
+                    cursos[id2].FilaEspera.Inserir(candidato);
+                }
+            }
+
+
+        }
+
+        public static void GerarArquivo(string lista)
+        {
+            string arq = @"C:\Users\gabri\OneDrive\Área de Trabalho\ArquivoSaida.txt";
+
+            using (StreamWriter listaDeAprovados = new StreamWriter(arq))
+            {
+                listaDeAprovados.Write(lista);
+            }
+
+        }
+    }
+}
